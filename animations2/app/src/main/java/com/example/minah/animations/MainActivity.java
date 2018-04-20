220a6b7a8a3f8d21868b440918d571c26d093fb7
nabimanya arnold <nabimanyaarnold@gmail.com>
2018-04-20 18:45:15 +0300

    work

---
+++ animations2/app/src/main/java/com/example/minah/animations/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.minah.animations;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private static int WELCOME_TIMEOUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent welcome = new Intent (MainActivity.this, SecondActivity.class);
+                startActivity(welcome);
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+                finish();
+            }
+        }, WELCOME_TIMEOUT);
+    }
+}
---
+++ animations2/app/src/main/java/com/example/minah/animations/WELCOME_TIMEOUT.java
@@ -0,0 +1,8 @@
+package com.example.minah.animations;
+
+/**
+ * Created by miNaH on 4/17/2018.
+ */
+
+class WELCOME_TIMEOUT {
+}
